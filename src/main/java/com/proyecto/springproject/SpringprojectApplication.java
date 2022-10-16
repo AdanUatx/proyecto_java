a829ff5f9a3070e537505dfa529014f35741263a
AdanUATx <20191445@uatx.mx>
2022-10-16 11:19:17 -0500

    Cambios

--- src/main/java/com/proyecto/springproject/SpringprojectApplication.java
+++ src/main/java/com/proyecto/springproject/SpringprojectApplication.java
@@ -2,6 +2,9 @@ package com.proyecto.springproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringprojectApplication {
@@ -10,4 +13,15 @@ public class SpringprojectApplication {
 		SpringApplication.run(SpringprojectApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings( CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS").allowedOrigins("*")
+						.allowedHeaders("*").maxAge(-1)   // add maxAge
+						.allowCredentials(false);
+			}
+		};
+	}
 }
